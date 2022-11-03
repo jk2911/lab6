4ef76a5470a8299096d7e6d7ab87f2573735dcd3
Maxim Goy <maksgoy2911@gmail.com>
2022-11-03 12:02:41 +0300

    add landscape orientation

--- app/src/main/java/maxim/goy/lab6/MainActivity.java
+++ app/src/main/java/maxim/goy/lab6/MainActivity.java
@@ -1,6 +1,8 @@
 package maxim.goy.lab6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -8,15 +10,29 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import maxim.goy.lab6.Fragment.DetailFragment;
 import maxim.goy.lab6.Fragment.ListEventFragment;
 import maxim.goy.lab6.Model.Event;
 
 public class MainActivity extends AppCompatActivity implements ListEventFragment.OnFragmentSendDataListener {
 
+    FragmentManager fragmentManager = getSupportFragmentManager();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //fragmentManager.beginTransaction().setReorderingAllowed(true).
+        //        replace(R.id.fragment_container_view, ListEventFragment.class, savedInstanceState).
+          //      commit();
+
+        /*if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            fragmentManager.beginTransaction().setReorderingAllowed(true).
+                    replace(R.id.fragment_item_event, DetailFragment.class, savedInstanceState).
+                    commit();
+            fragmentManager.popBackStack();
+        }*/
     }
 
     @Override
