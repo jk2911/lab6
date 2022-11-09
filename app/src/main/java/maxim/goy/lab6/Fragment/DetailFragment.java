8e299615ce94aa5799341b509718ce31386f754d
maxim <maksgoy2911@gmail.com>
2022-11-09 17:15:32 +0300

    positioning of elements

--- app/src/main/java/maxim/goy/lab6/Fragment/DetailFragment.java
+++ app/src/main/java/maxim/goy/lab6/Fragment/DetailFragment.java
@@ -4,15 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import maxim.goy.lab6.Model.Event;
+import maxim.goy.lab6.Model.Image;
 import maxim.goy.lab6.R;
 
 public class DetailFragment extends Fragment {
-    TextView name;
+    TextView name, description;
+    ImageView image;
 
     View view;
 
@@ -20,10 +23,14 @@ public class DetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.detail_fragment, container, false);
         name = view.findViewById(R.id.name);
+        description=view.findViewById(R.id.description);
+        image=view.findViewById(R.id.image);
         return view;
     }
 
     public void setSelectedItem(Event event) {
-        name.setText(event.getNameImage());
+        name.setText(event.name);
+        description.setText(event.description);
+        Image.getInstance().loadImageFromStorage(image,event.pathImages);
     }
 }
