61b1d85fd560e5646e6994887d69bc550cce9304
Maxim Goy <maksgoy2911@gmail.com>
2022-11-14 10:48:16 +0300

    change DatabaseHelper

--- app/src/main/java/maxim/goy/lab6/DB/DatabaseHelper.java
+++ app/src/main/java/maxim/goy/lab6/DB/DatabaseHelper.java
@@ -7,16 +7,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper  extends SQLiteOpenHelper {
     public final static String DATABASE_NAME = "events.db";
     public final static int SCHEMA = 1;
+    final static String TABLE = "events";
+
+    public final static String COLUMN_NAME = "name";
+    public final static String COLUMN_ID = "_id";
+    public final static String COLUMN_DESCRIPTION = "description";
+    public final  static String COLUMN_CALENDAR = "date";
+    public final static String COLUMN_PATH_IMAGE= "path_image";
+
+
     public DatabaseHelper(Context context){
         super(context, DATABASE_NAME, null, SCHEMA);
     }
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("CREATE TABLE " + TABLE + " (" + COLUMN_ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_NAME
+                + " TEXT, " + COLUMN_DESCRIPTION + " TEXT"+ COLUMN_PATH_IMAGE + " TEXT);");
+        // добавление начальных данных
+        //db.execSQL("INSERT INTO "+ TABLE +" (" + COLUMN_NAME
+                //+ ", " + COLUMN_YEAR  + ") VALUES ('Том Смит', 1981);");
     }
-
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase db, int oldVersion,  int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE);
+        onCreate(db);
     }
 }
